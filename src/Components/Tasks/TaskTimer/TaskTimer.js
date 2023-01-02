e4acbf7d43cfb40867f93f04b49b9f30e5bc72f1
Shan Pisciotta <shanpisciotta.pro@gmail.com>
2023-01-02 16:04:14 +0100

    Finalisation de l'application

---
+++ src/Components/Tasks/TaskTimer/TaskTimer.js
@@ -0,0 +1,36 @@
+import { useContext, useEffect } from "react";
+import { TasksContext } from "../../../Contexts/TasksContext";
+import useTimeParser from "../../../Hooks/useTimeParser";
+import useTimer from "../../../Hooks/useTimer";
+import Button from "../../UI/Button/Button";
+import style from "./TaskTimer.module.css"
+
+const TaskTimer = ({index, onCloseModal}) => {
+
+    const {time, startTimer, stopTimer} = useTimer();
+    const {parseSecondsToHms} = useTimeParser();
+
+    const {editTask} = useContext(TasksContext);
+
+    useEffect(() => {
+        startTimer();
+        return () => { // Composant will unmount
+            stopTimer();
+        }
+    }, [])
+
+    const handleStopTimer = () => {
+        const savedTime = stopTimer();
+        editTask({taskIndex: index, task: {time: savedTime}});
+        onCloseModal();
+    }
+
+    return (
+        <div className={style["timer-container"]}>
+            <p className={style.timer}>{parseSecondsToHms(time)}</p>
+            <Button onClick={handleStopTimer}>Stop</Button>
+        </div>
+    )
+};
+
+export default TaskTimer;
--- src/Components/Tasks/TasksTable/TaskRow.js
+++ src/Components/Tasks/TasksTable/TaskRow.js
@@ -1,14 +1,19 @@
 import { useContext, useState } from "react";
 import { TasksContext } from "../../../Contexts/TasksContext";
+import useTimeParser from "../../../Hooks/useTimeParser";
 import Button from "../../UI/Button/Button";
 import Modal from "../../UI/Modal/Modal";
 import TaskForm from "../TaskForm";
+import TaskTimer from "../TaskTimer/TaskTimer";
 
 const TaskRow = ({task, index}) => {
 
     const [isEditTaskModalOpen, setIsEditTaskModalOpen] = useState(false)
+    const [isTimerModalOpen, setIsTimerModalOpen] = useState(false)
 
     const {removeTask, toggleTaskIsDone} = useContext(TasksContext)
+
+    const {parseSecondsToHms} = useTimeParser()
 
     const handleDeleteTask = () => {
         removeTask(index);
@@ -35,15 +40,19 @@ const TaskRow = ({task, index}) => {
                     {task.createdAt.toLocaleDateString()}
                 </td>
                 <td>
-                    {task.time}
+                    {task.time && parseSecondsToHms(task.time)}
                 </td>
                 <td style={{display:"flex", gap:4, justifyContent: "end"}}>
+                    <Button onClick={() => setIsTimerModalOpen(true)}>Launch Timer</Button>
                     <Button variant="danger" onClick={handleDeleteTask}>Delete</Button>
                     <Button onClick={() => setIsEditTaskModalOpen(true)}>Edit</Button>
                 </td>
             </tr>
             <Modal isOpen={isEditTaskModalOpen} setIsOpen={setIsEditTaskModalOpen} title={task.title}>
                 <TaskForm closeModal={() => setIsEditTaskModalOpen(false)} value={{title:task.title, description: task.description}} index={index}/>
+            </Modal>
+            <Modal isOpen={isTimerModalOpen} setIsOpen={setIsTimerModalOpen} title={task.title}>
+                <TaskTimer index={index} onCloseModal={() => setIsTimerModalOpen(false)}/>
             </Modal>
         </>
     )
